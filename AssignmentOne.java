c6a5e4ff549663c6806a3941ec59f3ef0f406a71
marara02 <57908460+marara02@users.noreply.github.com>
2020-04-18 01:07:28 +0600

    Add files via upload

---
+++ AssignmentOne.java
@@ -0,0 +1,26 @@
+package com.company;
+import java.io.FileNotFoundException;
+import java.nio.file.FileAlreadyExistsException;
+import java.util.Scanner;
+import java.io.File;
+
+public class AssignmentOne {
+
+    public static void main(String[] args) throws FileAlreadyExistsException, FileNotFoundException {
+	// write your code here
+        Shape s = new Shape();
+        Scanner file = new Scanner(new File("C:\\Users\\ww\\IdeaProjects\\hw\\src\\com\\company\\file.txt"));
+        while(file.hasNextDouble()){
+            double x = file.nextDouble();
+            double y = file.nextDouble();
+            Point a = new Point(x,y);
+            s.addPoint(a);
+            //s.addPoint();
+            
+        }
+        System.out.println(s.calculatePerimeter());
+        System.out.println(s.LongestSide());
+        System.out.println(s.AverageSide());
+        //s.getPoints();
+    }
+}
